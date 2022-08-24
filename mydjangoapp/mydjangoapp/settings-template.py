f557fbcb721ae19b2749c5c466f8f3062e4fee1b
Biluca <vinicios.biluca@loadsmart.com>
2022-08-23 21:15:35 -0300

    Fixing Security Leak

--- mydjangoapp/mydjangoapp/settings-template.py
+++ mydjangoapp/mydjangoapp/settings-template.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%c5r+dy^dl_iyhs@_zm2jp9q++y9en3d$f&z%)a7yuvm(k)o&'
+SECRET_KEY = # YOUR SECRETE KEY HERE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
